83d8e05b0cb55eeaa83aca2a0ba02aad5225f50e
Chimaa123 <oyuhaanaa@gmail.com>
2024-01-12 15:03:24 +0800

    v2.0.4

--- dist/esm/types/cluster/ClusterLayer.d.ts
+++
@@ -1,2 +0,0 @@
-import { ClusterLayerProps } from "./cluster.type";
-export declare const ClusterLayer: ({ points, clusterOptions, options, onClick, onOver, onSourceCreated, clusterStyle, }: ClusterLayerProps) => null;
