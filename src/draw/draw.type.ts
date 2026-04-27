5b3e342c300e90b1d199017d05206bfe3700f5f2
치마지 <oyuhaanaa@gmail.com>
2026-04-27 09:38:15 +0800

    fix: draw to hook

--- src/draw/draw.type.ts
+++ src/draw/draw.type.ts
@@ -1,18 +1,23 @@
-import { Options } from "ol/interaction/Draw";
+import * as ol from "ol";
+import { DrawEvent, Options } from "ol/interaction/Draw";
 import { Coordinate } from "ol/coordinate";
 import VectorSource from "ol/source/Vector";
 import { StyleLike } from "ol/style/Style";
+import BaseEvent from "ol/events/Event";
 
 export type DrawProps = {
   /**
    * unique id to get overlay container element
    */
+  map?: ol.Map;
   drawStyle?: StyleLike;
   drawnStyle?: StyleLike;
-  onSourceCreated?: (source: VectorSource) => void;
-  onDrawStart?: (coordinate: Coordinate, event: any) => void;
-  onDrawEnd?: (event: any) => void;
-  onGetPointsInsidePolygon?: (coordinate: Coordinate, event: any) => void;
-  onDrawAbort?: (coordinate: Coordinate, event: any) => void;
   options?: Options;
+  visible?: boolean;
+  /*Listeners*/
+  onSourceCreated?: (source: VectorSource) => void;
+  onDrawStart?: (event: BaseEvent | Event) => void;
+  onDrawEnd?: (event: DrawEvent) => void;
+  onDrawAbort?: (event: BaseEvent | Event) => void;
+  onGetPointsInsidePolygon?: (coordinate: Coordinate, event: any) => void;
 };
