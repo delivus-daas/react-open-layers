d2c384d4233c2e9b4db869e626fdfd4b1416139b
치마지 <oyuhaanaa@gmail.com>
2026-05-20 11:21:28 +0800

    refactor: separate geolocation

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export { default as OpenLayers } from "./OpenLayers";
 export { useCluster } from "./cluster";
 export { useLayer } from "./layer";
+export { useGeolocation } from "./geolocation";
 export { CustomOverlay } from "./overlay";
 export { Controller } from "./control";
 export { usePolygonDraw } from "./draw";
